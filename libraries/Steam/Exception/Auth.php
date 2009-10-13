4fb270745cad68ad4f284b04500847088ee43b7b
shaddy.zeineddine <shaddy.zeineddine@57f7067e-acbe-11dd-934b-df5d59850f69>
2009-10-13 08:30:34 +0000

    added authentication/authorization exception
    git-svn-id: https://steam-fw.googlecode.com/svn/trunk@66 57f7067e-acbe-11dd-934b-df5d59850f69

---
+++ libraries/Steam/Exception/Auth.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Steam Auth Exception
+ *
+ * This exception is thrown when a user attempts to access something without
+ * being authorized.
+ *
+ * Copyright 2008-2009 Shaddy Zeineddine
+ *
+ * This file is part of Steam, a PHP application framework.
+ *
+ * Steam is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Steam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category Frameworks
+ * @package Steam
+ * @copyright 2008-2009 Shaddy Zeineddine
+ * @license http://www.gnu.org/licenses/gpl.txt GPL v3 or later
+ * @link http://code.google.com/p/steam-fw
+ */
+
+require_once 'Steam/Exception.php';
+
+class Steam_Exception_Auth extends Steam_Exception
+{
+    protected function setMessage(&$message)
+    {
+        $message = gettext('Unauthorized.');
+    }
+}
+
+?>
