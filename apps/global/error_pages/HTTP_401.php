9a446dbfff2508e9c0a5f62c74ba5fcf673d7c89
shaddy.zeineddine <shaddy.zeineddine@57f7067e-acbe-11dd-934b-df5d59850f69>
2010-10-29 17:57:48 +0000

    added global app files
    git-svn-id: https://steam-fw.googlecode.com/svn/trunk@74 57f7067e-acbe-11dd-934b-df5d59850f69

---
+++ apps/global/error_pages/HTTP_401.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html>
+    <head>
+        <title>401 Unauthorized</title>
+    </head>
+    <body>
+        <h1>Unauthorized</h1>
+        <p>You are not authorized to access <?php echo $_SERVER['REQUEST_URI']; ?>.</p>
+    </body>
+</html>
+
