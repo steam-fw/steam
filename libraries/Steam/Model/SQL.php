c7b0a4cc2db188f9c2ccb6043700a28f17d7c626
shaddy.zeineddine <shaddy.zeineddine@57f7067e-acbe-11dd-934b-df5d59850f69>
2010-12-21 06:53:58 +0000

    added support for specifying order by in query string rather than query/request xml (still missing: max_items, start_index)
    git-svn-id: https://steam-fw.googlecode.com/svn/trunk@78 57f7067e-acbe-11dd-934b-df5d59850f69

--- libraries/Steam/Model/SQL.php
+++ libraries/Steam/Model/SQL.php
@@ -1,10 +1,39 @@
 <?php
+/**
+ * Steam SQL Helper Class
+ *
+ * This class simplifies retrieval from SQL databases
+ *
+ * Copyright 2008-2010 Shaddy Zeineddine
+ *
+ * This file is part of Steam, a PHP application framework.
+ *
+ * Steam is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Steam is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category Frameworks
+ * @package Steam
+ * @copyright 2008-2010 Shaddy Zeineddine
+ * @license http://www.gnu.org/licenses/gpl.txt GPL v3 or later
+ * @link http://code.google.com/p/steam-fw
+ */
 
 namespace Steam\Model;
 
 class SQL
 {
     private $max_items = NULL;
+    private $order_by  = NULL;
     private $params = array();
     private $request;
     private $response;
@@ -28,11 +57,20 @@ class SQL
         {
             $this->max_items = $this->params['max-items'];
             unset($this->params['max-items']);
-            
         }
         elseif (!empty($this->request->max_items))
         {
             $this->max_items = (int) $this->request->max_items;
+        }
+        
+        if (isset($this->params['order-by']))
+        {
+            $this->order_by = $this->params['order-by'];
+            unset($this->params['order-by']);
+        }
+        elseif (!empty($this->request->order_by))
+        {
+            $this->order_by = (int) $this->request->order_by;
         }
     }
     
@@ -185,12 +223,13 @@ class SQL
     
     public function order(&$select)
     {
-        if (!$this->request->order_by)
+        if (is_null($this->order_by))
         {
             return;
         }
         
-        $select->order($this->request->order_by);
+        $select->reset(\Zend_Db_Select::ORDER);
+        $select->order(explode(',', (string) $this->order_by));
     }
     
     private function add_results(&$select)
