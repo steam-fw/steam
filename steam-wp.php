d27a0971cfc4f15f08380e1616aea2ca8bc7ce8e
shaddy.zeineddine <shaddy.zeineddine@57f7067e-acbe-11dd-934b-df5d59850f69>
2010-12-29 02:06:49 +0000

    added architectural strategies
    git-svn-id: https://steam-fw.googlecode.com/svn/trunk@87 57f7067e-acbe-11dd-934b-df5d59850f69

--- steam-wp.php
+++ steam-wp.php
@@ -3,80 +3,72 @@
 Plugin Name: SteamWP
 Plugin URI: http://code.google.com/p/steam-fw/
 Description: Steam is a php web application framework which, when combined with WordPress, provides alternative facilities for URI mapping, data manipulation, and templating while providing other useful functionality.
-Version: 0.1.0
+Version: 0.2.0
 Author: Shaddy Zeineddine, Braydon Fuller
 */
 
-function steam_initialize()
-{
-    include 'functions.php';
+include 'functions.php';
+include 'libraries/Steam.php';
 
-    include 'libraries/Steam.php';
-    
+add_action('init', function()
+{
     ob_start();
-
     Steam::load_config();
-
     Steam::initialize();
-
-    add_filter('rewrite_rules_array', 'steam_rewrites');
     
-    add_filter('query_vars', 'steam_query_vars');
-    
-    add_action('template_redirect', 'steam_template_redirect');
-}
-
-function steam_query_vars($wp_vars)
-{
-    $wp_vars[] = 'steam_action';
-    $wp_vars[] = 'steam_model';
-    $wp_vars[] = 'steam_view';
-    
-    return $wp_vars;
-}
-
-function steam_template_redirect()
-{
-    $type     = get_query_var('steam_resource_type');
-    $resource = get_query_var('steam_resource_name');
-    
-    if ($type and $resource)
+    add_filter('query_vars', function($wp_vars)
     {
-        Steam::set_request('wordpress', $type, $resource);
-        Steam::dispatch();
-        exit;
-    }
+        $wp_vars[] = 'steam_resource_type';
+        $wp_vars[] = 'steam_resource_name';
+        
+        return $wp_vars;
+    });
     
-}
+    add_action('template_redirect', function()
+    {
+        global $wp_rewrite;
+        $type     = get_query_var('steam_resource_type');
+        $resource = get_query_var('steam_resource_name');
+        
+        if ($type and $resource)
+        {
+            Steam::set_request('wordpress', $type, $resource);
+            Steam::dispatch();
+            exit;
+        }
+    });
+});
 
-function steam_rewrites($wp_rules)
-{
-    $steam_rules = array(
-        'actions/(.+)$' => 'index.php?steam_resource_type=action&steam_resource_name=$matches[1]',
-        'models/(.+)$'  => 'index.php?steam_resource_type=model&steam_resource_name=$matches[1]',
-        'views/(.+)$'   => 'index.php?steam_resource_type=view&steam_resource_name=$matches[1]',
-    );
-    
-    return array_merge($steam_rules, $wp_rules);
-}
-
-function steam_activation()
+register_activation_hook(__FILE__, function()
 {
     global $wp_rewrite;
-    add_filter('rewrite_rules_array', 'steam_rewrites');
+    add_filter('rewrite_rules_array', function($wp_rules)
+    {
+        $steam_rules = array(
+            'static/(.+)$'  => 'index.php?steam_resource_type=static&steam_resource_name=$matches[1]',
+            'actions/(.+)$' => 'index.php?steam_resource_type=action&steam_resource_name=$matches[1]',
+            'models/(.+)$'  => 'index.php?steam_resource_type=model&steam_resource_name=$matches[1]',
+            'views/(.+)$'   => 'index.php?steam_resource_type=view&steam_resource_name=$matches[1]',
+        );
+        
+        return $steam_rules + $wp_rules;
+    });
     $wp_rewrite->flush_rules();
-}
+});
 
-function steam_deactivation()
+register_deactivation_hook(__FILE__, function()
 {
     global $wp_rewrite;
+    add_filter('rewrite_rules_array', function($wp_rules)
+    {
+        unset($wp_rules['static/(.+)$']);
+        unset($wp_rules['actions/(.+)$']);
+        unset($wp_rules['models/(.+)$']);
+        unset($wp_rules['views/(.+)$']);
+        
+        return $wp_rules;
+    });
     $wp_rewrite->flush_rules();
-}
-
-add_action('init', 'steam_initialize');
-
-register_activation_hook(Steam::path('steam-wp.php'), 'steam_activation');
-
-register_deactivation_hook(Steam::path('steam-wp.php'), 'steam_deactivation');
+});
 
 ?>
